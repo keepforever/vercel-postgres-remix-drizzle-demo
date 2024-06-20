09c9dd1683fb4da3d1b937a74493bb774c86af0d
Brian Cilenti <briancilenti@gmail.com>
2024-06-19 23:42:38 -0400

    feat: new shell

--- app/components/app-shell.tsx
+++ app/components/app-shell.tsx
@@ -147,34 +147,7 @@ export function AppShell({ children }: { children: React.ReactNode }) {
                             ))}
                           </ul>
                         </li>
-                        <li>
-                          <div className="text-xs font-semibold leading-6 text-indigo-200">
-                            Your teams
-                          </div>
-                          <ul role="list" className="-mx-2 mt-2 space-y-1">
-                            {teams.map((team) => (
-                              <li key={team.name}>
-                                <NavLink
-                                  to={team.href}
-                                  className={({ isActive }) => {
-                                    return classNames(
-                                      isActive
-                                        ? "bg-indigo-700 text-white"
-                                        : "text-indigo-200 hover:bg-indigo-700 hover:text-white",
-                                      "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6"
-                                    );
-                                  }}
-                                >
-                                  <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-indigo-400 bg-indigo-500 text-[0.625rem] font-medium text-white">
-                                    {team.initial}
-                                  </span>
-                                  <span className="truncate">{team.name}</span>
-                                </NavLink>
-                                ```
-                              </li>
-                            ))}
-                          </ul>
-                        </li>
+
                         <li className="mt-auto">
                           <NavLink
                             to="#"
@@ -234,33 +207,7 @@ export function AppShell({ children }: { children: React.ReactNode }) {
                     ))}
                   </ul>
                 </li>
-                <li>
-                  <div className="text-xs font-semibold leading-6 text-indigo-200">
-                    Your teams
-                  </div>
-                  <ul role="list" className="-mx-2 mt-2 space-y-1">
-                    {teams.map((team) => (
-                      <li key={team.name}>
-                        <NavLink
-                          to={team.href}
-                          className={({ isActive }) => {
-                            return classNames(
-                              isActive
-                                ? "bg-indigo-700 text-white"
-                                : "text-indigo-200 hover:bg-indigo-700 hover:text-white",
-                              "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6"
-                            );
-                          }}
-                        >
-                          <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-indigo-400 bg-indigo-500 text-[0.625rem] font-medium text-white">
-                            {team.initial}
-                          </span>
-                          <span className="truncate">{team.name}</span>
-                        </NavLink>
-                      </li>
-                    ))}
-                  </ul>
-                </li>
+
                 <li className="mt-auto">
                   <NavLink
                     to="#"
