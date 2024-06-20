cb0a78583a0fc3c476ef95b8db44b896d04dc943
Brian Cilenti <briancilenti@gmail.com>
2024-06-20 13:05:10 -0400

    chore: layout agaion

--- app/pages/users/users-home.tsx
+++ app/pages/users/users-home.tsx
@@ -1,6 +1,5 @@
-import { Link, json, useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/react";
 import { db } from "~/db.server";
-import { useUserLayoutData } from "~/pages/users/hooks";
 
 export const loader = async () => {
   const users = await db.query.user.findMany();
@@ -9,9 +8,6 @@ export const loader = async () => {
 };
 
 export default function UsersHome() {
-  const loaderData = useLoaderData<typeof loader>();
-  const slashUsersMatch = useUserLayoutData();
-
   return (
     <div className="p-6 flex">
       <div className="p-2">
@@ -19,35 +15,7 @@ export default function UsersHome() {
           Users Home; rendered into the Outlet of <code>users-layout.tsx</code>
         </div>
 
-        {/* Users list with link */}
-        <div className="mt-4">
-          <h2 className="font-semibold">Users List</h2>
-          <ul>
-            {loaderData.users.map((user) => (
-              <li key={user.id} className="flex items-center gap-2 mb-2">
-                <span className="font-semibold">ID:</span> {user.id} -{" "}
-                <span className="font-semibold">Name:</span> {user.name}
-                <Link
-                  className="ml-2 text-blue-500 underline"
-                  to={`${user.id}`}
-                >
-                  Visit
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <h3 className="mt-4 font-semibold">Loader Data From Parent Route</h3>
-        <p>
-          Could have been any parent or child route that was matched on the
-          page, but, I targeted
-          <code>/users</code> in this case.
-        </p>
-
-        <pre>
-          {JSON.stringify(slashUsersMatch, null, 2) || "nothing to preview"}
-        </pre>
+        <h3>Select a user to view their details</h3>
       </div>
     </div>
   );
