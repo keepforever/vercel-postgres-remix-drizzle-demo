b9d5e16ec6929caaceb85a8d0413578dc22372c7
Brian Cilenti <briancilenti@gmail.com>
2024-06-22 15:38:02 -0400

    chore: tests stuff

--- tests/global-setup.ts
+++ tests/global-setup.ts
@@ -6,6 +6,15 @@ import { clerkSetup } from "@clerk/testing/playwright";
 async function globalSetup() {
   console.log("\n", `hello global setup run `, "\n");
   await clerkSetup();
+
+  if (
+    !process.env.E2E_CLERK_USER_USERNAME ||
+    !process.env.E2E_CLERK_USER_PASSWORD
+  ) {
+    throw new Error(
+      "Please provide E2E_CLERK_USER_USERNAME and E2E_CLERK_USER_PASSWORD environment variables."
+    );
+  }
 }
 
 export default globalSetup;
--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,10 +1,9 @@
-// import { setupClerkTestingToken } from "@clerk/testing/playwright";
+import { setupClerkTestingToken } from "@clerk/testing/playwright";
 import { test, expect } from "@playwright/test";
 
 test("Unauthenticated home view", async ({ page }) => {
-  //   await setupClerkTestingToken({ page });
-
   await page.goto("http://localhost:5173/");
+  await setupClerkTestingToken({ page });
 
   await expect(
     page.getByText("Please sign in to view this page")
